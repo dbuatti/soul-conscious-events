64eec2417d8c1138a5aa54f5fe2f79ba1d2cdb17
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-08-15 13:30:52 +1000

    [dyad] Set dark mode as the default theme. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes"; // Import ThemeProvider
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+  <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
     <App />
   </ThemeProvider>
 );
