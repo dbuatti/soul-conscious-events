f0ff1d41538def74b9a2326e99eb1b31fa29a55b
Daniele Buatti <danielebuatti@MacBook-Pro-7.local>
2025-08-28 12:35:03 +1000

    [dyad] Re-enabled map view toggle with updated message and removed dedicated map page from navigation. - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,50 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner";
+import { Layout } from "./components/Layout";
+import { SessionContextProvider } from "./components/SessionContextProvider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Index } from "./pages/Index";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
 import EventsList from "./pages/EventsList";
-import NotFound from "./pages/NotFound";
-import SubmitEvent from "./pages/SubmitEvent";
-import Contact from "./pages/Contact";
+import EventForm from "./pages/EventForm";
+import EventEdit from "./pages/EventEdit";
 import AdminPanel from "./pages/AdminPanel";
-import MapPage from "./pages/MapPage";
 import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
-import { SessionContextProvider } from "./components/SessionContextProvider";
-import Layout from "./components/Layout";
-import ScrollToTop from "./components/ScrollToTop";
-import EventEditPage from "./pages/EventEditPage";
-import EventDetailPage from "./pages/EventDetailPage";
-import About from "./pages/About";
-import DevSpace from "./pages/DevSpace";
-import CommunityGuidelines from "./pages/CommunityGuidelines";
-import MyEvents from "./pages/MyEvents";
-import MyBookmarks from "./pages/MyBookmarks"; // Import the new MyBookmarks page
+import Profile from "./pages/Profile";
+import EventDetails from "./pages/EventDetails";
+import NotFound from "./pages/NotFound"; // Import the NotFound component
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <ScrollToTop />
-        <SessionContextProvider>
-          <Layout>
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionContextProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Router>
             <Routes>
-              <Route path="/" element={<EventsList />} />
-              <Route path="/events/:id" element={<EventDetailPage />} />
-              <Route path="/submit-event" element={<SubmitEvent />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/community-guidelines" element={<CommunityGuidelines />} />
-              <Route path="/login" element={<Login />} />
-              <Route
-                path="/my-events"
-                element={
-                  <ProtectedRoute>
-                    <MyEvents />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/my-bookmarks"
-                element={
-                  <ProtectedRoute>
-                    <MyBookmarks />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/admin/panel"
-                element={
-                  <ProtectedRoute allowedEmail="daniele.buatti@gmail.com">
-                    <AdminPanel />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/dev-space"
-                element={
-                  <ProtectedRoute allowedEmail="daniele.buatti@gmail.com">
-                    <DevSpace />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/edit-event/:id"
-                element={
-                  <ProtectedRoute>
-                    <EventEditPage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/map" element={<MapPage />} />
-              <Route path="*" element={<NotFound />} />
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Index />} />
+                <Route path="about" element={<About />} />
+                <Route path="contact" element={<Contact />} />
+                <Route path="events" element={<EventsList />} />
+                <Route path="submit-event" element={<EventForm />} />
+                <Route path="edit-event/:id" element={<EventEdit />} />
+                <Route path="admin" element={<AdminPanel />} />
+                <Route path="login" element={<Login />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="events/:id" element={<EventDetails />} />
+                <Route path="*" element={<NotFound />} /> {/* Catch-all route for 404 */}
+              </Route>
             </Routes>
-          </Layout>
-        </SessionContextProvider>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+          </Router>
+        </TooltipProvider>
+      </SessionContextProvider>
+    </QueryClientProvider>
+  );
+}
 
 export default App;
